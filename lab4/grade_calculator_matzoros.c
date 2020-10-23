138f9e2a369155aeb670eb5883315283da498d1b
Alexander Matzoros <47158878+AlMatz@users.noreply.github.com>
2020-10-23 17:05:14 -0400

    Simple Command Interpreter and Grade Calculator

---
+++ lab4/grade_calculator_matzoros.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]) {
+	int x = 3; //# of chapters
+	int y = 2; //hws for each chapter
+	int students = 4; //how many students
+
+return 0;
+}
