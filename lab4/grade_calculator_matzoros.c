7a58f2582fa1240a4c4c4ddb59785bd894c2907e
Alexander Matzoros <47158878+AlMatz@users.noreply.github.com>
2020-10-23 17:12:01 -0400

    Grade Calculator

--- lab4/grade_calculator_matzoros.c
+++ lab4/grade_calculator_matzoros.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int main(int argc, char* argv[]) {
+int main() {
 	int x = 3; //# of chapters 
 	int y = 2; //hws for each chapter
 	int students = 4; //how many students
